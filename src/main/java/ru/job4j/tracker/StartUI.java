4f4adf05e3cdef3359455e11591c4bdc20cefe99
kilya113 <job4jstart@gmail.com>
2023-06-25 22:12:57 +0300

    3.3. Реализация класса StartUI. Изменение заявки [#500746  [#500746 #208620]]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -38,6 +38,15 @@ public class StartUI {
                 } else {
                     System.out.println("Хранилище еще не содержит заявок");
                 }
+            } else if (select == 2) {
+                System.out.println("=== Edit item ===");
+                System.out.print("Enter id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.replace(id, item);
+                System.out.println("Заявка изменена успешно.");
             } else if (select == 6) {
                 run = false;
             }
